ff3ef3b08a6904ae01475d5302fdf381234a956e
omerfaruk-sahin <omer.fs@hotmail.com>
2021-10-11 19:56:38 +0300

    Clean blog project V2

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
 const port = 3000
 const blog = { id: 1, title: "Blog title", description: "Blog description" }
+app.use(express.static('puplic'))
+
+app.set("view engine","ejs")
 app.get('/', (req, res) => {
-  res.send(blog)
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
