2c92c2811c1f05fc303c8b15325bf3a4175a1965
omerfaruk-sahin <omer.fs@hotmail.com>
2021-10-18 23:06:37 +0300

    Clean Blog Project V3

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const ejs = require('ejs')
+
+const mongoose =require('mongoose')
 const app = express()
+mongoose.connect('mongodb://localhost:27017/cleanblog-test-db')
+
+const Post=require('./models/Post')
 const port = 3000
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
 app.use(express.static('puplic'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.set("view engine","ejs")
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/',async (req, res) => {
+  const posts=await Post.find({})
+  
+  res.render('index',{posts})
 })
 app.get('/about', (req, res) => {
   res.render('about')
@@ -15,6 +24,10 @@ app.get('/about', (req, res) => {
 app.get('/add_post', (req, res) => {
   res.render('add_post')
 })
+app.post('/post',async(req,res)=>{
+  await Post.create(req.body)
+  res.redirect('/')
+} )
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
